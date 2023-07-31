8f90452fdda9018867a8c47f637f0f67bdbd101d
juanes G <jgalvez98@gmail.com>
2023-07-31 18:59:17 -0400

    weather api added

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+var titleEl = document.getElementById("title")
+var tempEl = document.getElementById("temp")
+var windEl = document.getElementById("wind")
+var humidityEl = document.getElementById("humidity")
+var fivedayForecast = document.getElementById("fiveday-forecast")
+let weather = {
+    apiKey: "7e68cdfa71c2c8c17187eb95320793b0",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=imperial&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".city").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°F";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " MPH";
+      document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+    
+  };
+  
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Miami");
+
+  
