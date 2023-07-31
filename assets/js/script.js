2614a3b6e300c4895bacb4e698f663b8f3310176
Isa Vega <imvega100@gmail.com>
2023-07-31 19:03:10 -0400

    added button

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var dropBtnEl = document.querySelector(".dropbtn")
+var dropMenuOne = document.getElementById("myDropdown")
+
+function dropMenuOptions() {
+    dropMenuOne.classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  dropBtnEl.addEventListener("click", dropMenuOptions)
