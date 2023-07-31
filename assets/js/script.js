84e84b38dbddd2a04d1c41bd0e9fd90b029a0456
juanes G <jgalvez98@gmail.com>
2023-07-31 19:49:24 -0400

    java fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// weather api //
 var titleEl = document.getElementById("title")
 var tempEl = document.getElementById("temp")
 var windEl = document.getElementById("wind")
@@ -58,4 +59,27 @@ let weather = {
   
   weather.fetchWeather("Miami");
 
+ //            //                  //                     //                 // 
+var dropBtnEl = document.querySelector(".dropbtn")
+var dropMenuOne = document.getElementById("myDropdown")
+
+function dropMenuOptions() {
+    dropMenuOne.classList.toggle("show");
+  }
   
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+
+  dropBtnEl.addEventListener("click", dropMenuOptions)
